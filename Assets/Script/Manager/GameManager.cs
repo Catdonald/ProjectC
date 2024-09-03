4229cb169162e4026affe8d944b5e5b6b91890e5
chloe050412 <92531179+chloe050412@users.noreply.github.com>
2024-09-03 11:53:14 +0900

    [오수안] PlayerPrefs 테스트 완료

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 240821 오수안
@@ -8,6 +9,15 @@ using UnityEngine;
 /// 게임 매니저를 통해 다른 매니저 인스턴스에 접근
 /// </summary>
 
+// prefab test 용 임시 데이터
+struct info
+{
+    public int id;
+    public string name;
+    public int level;
+    public int playerSpeed;
+    public int playerMaxStuff;
+}
 
 public class GameManager : MonoBehaviour
 {
@@ -15,8 +25,59 @@ public class GameManager : MonoBehaviour
 
     public PoolManager PoolManager;
 
+    public GameObject idText;
+    public GameObject levelText;
+    public GameObject speedText;
+
+
+    // 임시 데이터
+    info info;
     void Awake()
     {
         instance = this;
+    }
+
+    public void ChangeInfo()
+    {
+        info.id = Random.Range(1, 100);  // ID 초기화 추가
+        info.name = "suan";
+        info.level = Random.Range(1, 100);
+        info.playerSpeed = Random.Range(1, 100);
+        info.playerMaxStuff = Random.Range(1, 100);
+
+        ChangeText(); // 데이터 변경 시 텍스트 업데이트
+    }
+
+    public void GetData()
+    {
+        Debug.Log("Loads Data");
+       
+        info.id = PlayerPrefs.GetInt("id", 0);
+        info.name = PlayerPrefs.GetString("name", "default");
+        info.level = PlayerPrefs.GetInt("level", 0);
+        info.playerSpeed = PlayerPrefs.GetInt("speed", 0);
+        info.playerMaxStuff = PlayerPrefs.GetInt("maxStuff", 0);
+
+        ChangeText();
+    }
+
+    public void SaveData()
+    {
+        Debug.Log("Save Data");
+
+        PlayerPrefs.SetInt("id", info.id);
+        PlayerPrefs.SetString("name", info.name);
+        PlayerPrefs.SetInt("level", info.level);
+        PlayerPrefs.SetInt("speed", info.playerSpeed);
+        PlayerPrefs.SetInt("maxStuff", info.playerMaxStuff);
+
+        PlayerPrefs.Save();
+    }
+
+    void ChangeText()
+    {
+        idText.GetComponent<Text>().text = info.id.ToString(); 
+        levelText.GetComponent<Text>().text = info.level.ToString(); 
+        speedText.GetComponent<Text>().text = info.playerSpeed.ToString(); 
     }
 }
