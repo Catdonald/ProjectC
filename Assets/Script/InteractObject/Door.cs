1553b4f6c40563f109aa41b1890a8117f973fb14
chloe050412 <92531179+chloe050412@users.noreply.github.com>
2024-09-21 18:23:49 +0900

    [오수안] 문짝 중간에 멈추는 버그 수정

--- Assets/Script/InteractObject/Door.cs
+++ Assets/Script/InteractObject/Door.cs
@@ -5,54 +5,87 @@ using UnityEngine;
 public class Door : Interactable
 {
     [SerializeField] private bool isSomeoneIn;
-    [SerializeField] private MeshRenderer[] entranceMeshes;
+    [SerializeField] private MeshRenderer entranceMeshes;
     private float curRot = 0;
     private Coroutine doorCoroutine;
-    protected override void OnPlayerEnter()
-    {
-        doorCoroutine = StartCoroutine(OpenDoor());
-    }
-    protected override void OnPlayerExit()
-    {
 
-    }
-    protected override void OnCustomerEnter()
+    protected override void OnPlayerEnter(Collider other)
     {
-        doorCoroutine = StartCoroutine(OpenDoor());
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(OpenDoor(other));
+        }
     }
-    protected override void OnCustomerExit()
+    protected override void OnPlayerStay(Collider other)
     {
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(OpenDoor(other));
+        }
+    }
+    protected override void OnPlayerExit(Collider other)
+    {
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(CloseDoor(other));
+        }
+    }
+    protected override void OnCustomerEnter(Collider other)
+    {
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(OpenDoor(other));
+        }
+    }
+    protected override void OnCustomerStay(Collider other)
+    {
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(OpenDoor(other));
+        }
+    }
+    protected override void OnCustomerExit(Collider other)
+    {
+        if (doorCoroutine == null)
+        {
+            doorCoroutine = StartCoroutine(CloseDoor(other));
+        }
+    }
 
-    }
-
-    IEnumerator OpenDoor()
+    IEnumerator OpenDoor(Collider other)
     {
+        //Vector3 forwardDirection = other.transform.forward; // 플레이어가 바라보는 방향
+        //Vector3 rightDirection = Vector3.Cross(forwardDirection, -other.transform.right); // 오른쪽 방향 벡터
+
         while (curRot < 90)
         {
-            float rotationThisFrame = Time.deltaTime * 50; // 초당 15도 회전
+            float rotationThisFrame = Time.deltaTime * 400;
             curRot += rotationThisFrame;
 
-            entranceMeshes[0].transform.Rotate(-Vector3.up, rotationThisFrame);
-            entranceMeshes[1].transform.Rotate(Vector3.up, rotationThisFrame);
+            entranceMeshes.transform.Rotate(other.transform.up, rotationThisFrame);
 
             yield return null;
         }
+
+        curRot = 90;
 
         doorCoroutine = null;
     }
 
-    IEnumerator CloseDoor()
+    IEnumerator CloseDoor(Collider other)
     {
+
         while (curRot > 0)
         {
-            float rotationThisFrame = Time.deltaTime * 50; // 초당 15도 회전
-            curRot += rotationThisFrame;
+            float rotationThisFrame = Time.deltaTime * 400;
+            curRot -= rotationThisFrame;
 
-            entranceMeshes[0].transform.Rotate(Vector3.up, rotationThisFrame);
-            entranceMeshes[1].transform.Rotate(-Vector3.up, rotationThisFrame);
+            entranceMeshes.transform.Rotate(-other.transform.up, rotationThisFrame);
 
             yield return null;
         }
+
+        curRot = 0;
 
         doorCoroutine = null;
     }
